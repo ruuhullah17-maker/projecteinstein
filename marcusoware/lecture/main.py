2e48a5107cbb5ec126ca1006dca993d285ad7c56
demarcuscode <demarcuscode@Kali.Kali>
2026-04-04 14:11:28 +0000

    added lecture folder

---
+++ marcusoware/lecture/main.py
@@ -0,0 +1,51 @@
+# # # first task
+# # name = "Marcus "
+# # num =4
+# # name = input("What is your name ? ")
+# # print(f"hello {name.upper()}")
+
+# # def take_name():
+#     # fname,lname = input("what is your fullname: ").split(" ")
+#     # return f"this is your full name  {fname } {lname}"
+
+# # print(take_name())
+
+
+# # check the day function 
+# def check_day():
+
+#     # take the users day input 
+#     day = input("What day ? ")
+
+#     if day == "monday":
+#        return "Monday"
+
+#     elif day == "Tueday":
+#         return "Tuesday"
+    
+#     else:
+#         return "invalid input"
+
+# print(check_day())
+
+
+# def calculator():
+#     a,b,c = input("Enter the values first , then the rule : " ).split(" ")
+
+#     if c == "add":
+#         return a + b
+#     elif c == "sub":
+#         return a - b
+#     elif c == "mul":
+#         return a * b
+#     elif c == 'div':
+#         return a / b
+#     else:
+#         return "invalid input"
+
+# print(calculator())
+
+n = 0 
+while  n < 5:
+    print("hello")
+    n += 1
