d756eaecaf9d2f06bd4a2e1ed23344d618bb5ebb
12sedricktieko-cmyk <12sedricktieko@gmail.com>
2026-04-06 20:19:35 +0000

    sedrick file

---
+++ sedricktieko/practicals/sedrick.py
@@ -0,0 +1,10 @@
+# example one
+#code to print full name
+def full_name():
+    first_name= input("what is your first name: ").upper()
+    
+    second_name=input("what is your second name: ").upper()
+
+    full_name = first_name +" "+ second_name
+    return full_name
+print(full_name())
