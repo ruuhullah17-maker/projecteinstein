ff4e5672b25a86bbbb5009e3eec8612000223af1
ruuhullah17-maker <ruuhullah17@gmail.com>
2026-04-10 17:27:38 +0000

    added a new file

--- OneDrive/Desktop/python_class/projecteinstein/issahdasmani/main.py
+++ OneDrive/Desktop/python_class/projecteinstein/issahdasmani/main.py
@@ -8,3 +8,24 @@ def whileloop():
         print("meow")
         n+=1
 whileloop()
+
+
+def days_of_the_week(day):
+    if day =="Monday":
+        return("Monday")
+    elif day =="Tuesday":
+        return("Tuesday")
+    elif day =="Wednesday":
+        return("Wednesday")
+    elif day =="Thursday":
+        return("Thursday")
+    elif day =="Friday":
+        return("Friday")
+    elif day =="Saturday":
+        return("Saturday")
+    elif day =="Sunday":
+        return("Sunday")
+    else:
+       return "invalid input"
+print(days_of_the_week("Friday"))
+    
