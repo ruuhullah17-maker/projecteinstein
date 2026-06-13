e8f7147cafafedcd31ff9d77fd582cbddefa11ac
ruuhullahissah-17 <ruuhullah17@gmail.com>
2026-06-13 16:17:22 +0000

    added a new file

---
+++ OneDrive/Desktop/python_class/projecteinstein/issahdasmani/practical/practice/pract.py
@@ -0,0 +1,33 @@
+print("#")
+print("#")
+print("#")
+
+for i in range(3):
+    print("#")
+
+
+    def main():
+        print_column(3)
+
+    def print_column(height):
+        for i in range(height):
+            print("#")
+
+    main()
+
+
+    def main():
+        print_row(4)
+    def print_row(width):
+        print("?" * width)
+
+    main()
+
+
+    def main():
+        print_square(3)
+    def print_square(size):
+        for i in range(size):
+         for j in range(size):
+             print(brick)
+             print("#", end="")
